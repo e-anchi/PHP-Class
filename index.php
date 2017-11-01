2431c69d255a116c6c6b7f93bfcb92cc9a929ec3
e-anchi <y0980601000@gmail.com>
2017-11-01 14:08:44 +0800

    0921 刪除html 改用 php 來做網頁

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello | PHP</title>
+</head>
+<body>
+<h1>Hello World~ from php</h1>
+<h1>哈囉~ from php</h1>
+<?php
+    echo "This is after hypertext preprocessor!"
+?>
+</body>
+</html>
