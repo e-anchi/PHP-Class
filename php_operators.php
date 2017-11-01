5c36638341f43b8af7482072f94ad4342eabeec2
e-anchi <y0980601000@gmail.com>
2017-11-01 14:24:40 +0800

    0928 PHP 基礎語法練習

---
+++ php_operators.php
@@ -0,0 +1,22 @@
+<?php
+$x=10;
+$y=6;
+echo ($x + $y)."<br>"; // 输出 16
+echo ($x - $y)."<br>"; // 输出 4
+echo ($x * $y)."<br>"; // 输出 60
+echo ($x / $y)."<br>"; // 输出 1.6666666666667
+echo ($x % $y)."<br>"; // 输出 4
+?>
+
+<?php
+$a = "Hello";
+$b = $a . " world!";
+echo $b; // 输出 Hello world!
+
+echo "<br>";
+
+$x="Hello";
+//$x .= " world!";
+$x = $x . " world!";
+echo $x; // 输出 Hello world!
+?>
